def97a5d461e6f7c1f09a5d4aa57c466367195a1
Dustin C <pixel.galaxies@gmail.com>
2017-02-25 00:11:02 -1000

    Add files via upload
    crescendo v2.0

---
+++ crescendo.js
@@ -0,0 +1,51 @@
+// Javascript for Crescendo
+// Author: Dustin Clark 2016
+
+//Shout out to Edward Meyer for his big brain and helping me when I was stuck.
+
+
+
+//displays dropdown items and switches icon
+function menuDrop() {
+	document.getElementsByClassName("dropdown")[0].style.display="block";
+	
+	document.getElementsByClassName("menu-icon")[0].style.display="none";
+	
+	document.getElementsByClassName("close-icon")[0].style.display="inherit";
+}
+
+
+//closes dropdown items and switches icons
+function menuClose() {
+	document.getElementsByClassName("dropdown")[0].style.display="none";
+	
+	document.getElementsByClassName("menu-icon")[0].style.display="inherit";
+	
+	document.getElementsByClassName("close-icon")[0].style.display="none";
+}
+
+//this function is called every time the window is resized
+//will hide everything that needs to be hidden based on window size
+function hide() {
+	if (window.innerWidth < 768) {
+		document.getElementsByClassName("menu-icon")[0].style.display="inherit";
+		
+		document.getElementsByClassName("close-icon")[0].style.display="none";
+		
+		document.getElementsByClassName("dropdown")[0].style.display="none";
+		
+		document.getElementsByClassName("nav-links")[0].style.display="none";
+	}
+	else {
+		document.getElementsByClassName("menu-icon")[0].style.display="none";
+		
+		document.getElementsByClassName("close-icon")[0].style.display="none";
+		
+		document.getElementsByClassName("dropdown")[0].style.display="none";
+		
+		document.getElementsByClassName("nav-links")[0].style.display="inherit";
+	}
+	
+}
+
+window.addEventListener("resize", hide);
